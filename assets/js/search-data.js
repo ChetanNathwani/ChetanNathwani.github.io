848d89d342d35977944c1b163dadcec8c3e77044
ChetanNathwani <82047572+ChetanNathwani@users.noreply.github.com>
2025-07-12 15:39:07 +0000

    Deploying to gh-pages from @ ChetanNathwani/ChetanNathwani.github.io@2ae3d12b1225a5b3434356042d09d3311ef9e064 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%68%65%74%61%6E.%6E%61%74%68%77%61%6E%69@%65%61%70%73.%65%74%68%7A.%63%68", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -509,6 +509,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/cnathwani", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,14 +529,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Zi1PnrQAAAAJ&hl=en", "_blank");
         },
       },{
       id: 'light-theme',
