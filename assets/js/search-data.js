f787717ad7ed90ffd6ae20dd8ce1945d28ab5c97
ChetanNathwani <82047572+ChetanNathwani@users.noreply.github.com>
2025-07-15 13:59:48 +0000

    Deploying to gh-pages from @ ChetanNathwani/ChetanNathwani.github.io@55aaaf9d2d5d229353270665aee37f4632c5ad99 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "most publications are available open access, please email me if you are unable to gain access to any. for the most up to date list please visit my [Google Scholar page](https://scholar.google.com/citations?user=Zi1PnrQAAAAJ&amp;hl=en).",
+          description: "most publications are available open access, please email me if you are unable to gain access to any.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
