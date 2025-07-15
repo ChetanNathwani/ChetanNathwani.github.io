f6625b3fd0759b6c0a51da9f10cff4bdc347f04b
ChetanNathwani <82047572+ChetanNathwani@users.noreply.github.com>
2025-07-15 13:18:02 +0000

    Deploying to gh-pages from @ ChetanNathwani/ChetanNathwani.github.io@6f96525ae67f50e93497f1f91214b1012a5ac120 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -66,12 +66,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-gave-an-invited-talk-at-the-goldschmidt-conference-in-prague",
+      },{id: "news-i-gave-an-invited-talk-at-the-goldschmidt-conference-in-prague",
           title: 'I gave an invited talk at the Goldschmidt conference in Prague',
           description: "",
           section: "News",},{id: "projects-project-1",
